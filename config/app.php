26fab3df69e9e799203500b58c665191ed92c0a1
imzhi <yxz_blue@126.com>
2019-11-22 17:56:55 +0800

    require imzhi/super:dev-master

--- config/app.php
+++ config/app.php
@@ -177,7 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Imzhi\Super\SuperServiceProvider::class,
+        // Imzhi\Super\SuperServiceProvider::class,
 
     ],
 
@@ -227,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Super' => Imzhi\Super\Facades\Super::class,
+        // 'Super' => Imzhi\Super\Facades\Super::class,
 
     ],
 
